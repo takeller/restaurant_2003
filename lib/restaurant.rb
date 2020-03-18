5d192af5a4e2f5c9518ae2a6e40de253e6a56f7c
Taylor Keller <takeller17@gmail.com>
2020-03-18 13:18:25 -0600

    Complete iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,4 +6,14 @@ class Restaurant
     @name = name
     @dishes = []
   end
+
+  def closing_time(hours_open_for)
+    opening_time_as_integer = opening_time.to_i
+    closing_time_as_integer = opening_time_as_integer + hours_open_for
+    closing_time_as_integer.to_s + ":00"
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end 
 end
